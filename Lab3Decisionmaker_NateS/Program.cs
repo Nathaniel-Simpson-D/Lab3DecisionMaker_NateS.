bdf41ced636d0a62db49ec68a344a15eb3c5b995
Nathaniel-Simpson-D <simpson.n.0308@gmail.com>
2019-07-17 09:35:00 -0400

    added loop & cleaned up IF else(s)

--- Lab3Decisionmaker_NateS/Program.cs
+++ Lab3Decisionmaker_NateS/Program.cs
@@ -13,40 +13,49 @@ namespace Lab3Decisionmaker_NateS
             //promt for name
             Console.WriteLine("Hello, what is your name?");
             string userName = Console.ReadLine();
-            //Prompt for num
-            int userNum = 101;
-            while (userNum > 100 || userNum < 1)
+
+            string con = "Y";
+
+            while (con != "N" && con != "n")
             {
-                Console.WriteLine("Please enter a number between 1 and 100");
-                string userIn = Console.ReadLine();
-                try { userNum = int.Parse(userIn); }
-                catch { userNum = 101; }
+                //Prompt for num
+                int userNum = 101;
+                while (userNum > 100 || userNum < 1)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100");
+                    string userIn = Console.ReadLine();
+                    try { userNum = int.Parse(userIn); }
+                    catch { userNum = 101; }
 
-                if (userNum > 100 || userNum < 1)
-                { Console.WriteLine($"Please enter a valid number"); }
-                else
-                { Console.WriteLine($"{userName}, you have chosen {userNum}."); }
+                    if (userNum > 100 || userNum < 1)
+                    { Console.WriteLine($"Please enter a valid number"); }
+                    else
+                    { Console.WriteLine($"{userName}, you have chosen {userNum}."); }
+                }
 
+                //validate
+                //Calc
+
+                /*if (userNum % 2 != 0 && userNum > 60)
+                { Console.WriteLine($"{userNum} Odd"); }
+                else */
+                if (userNum % 2 == 0 && userNum > 60)
+                { Console.WriteLine($"{userNum} Even."); }
+                else if (userNum % 2 == 0 && userNum >= 26)
+                { Console.WriteLine($"Even."); }
+                else if (userNum % 2 == 0 && userNum >= 2)
+                { Console.WriteLine("Even and less than 25"); }
+                else { Console.WriteLine($"{userNum} Odd."); }
+
+                Console.WriteLine($"{userName}, would you like to continue? (Y/N).");
+                con = Console.ReadLine();
 
             }
-
-            //validate
-            //Calc
-
-            if (userNum % 2 != 0 && userNum > 60)
-            { Console.WriteLine($"{userNum} Odd"); }
-            else if (userNum % 2 == 0 && userNum >= 2 && userNum <= 25)
-            { Console.WriteLine("Even and less than 25"); }
-            else if (userNum % 2 == 0 && userNum >= 26 && userNum <= 60)
-            { Console.WriteLine($"Even."); }
-            else if (userNum % 2 == 0 && userNum > 60)
-            { Console.WriteLine($"{userNum} Even."); }
-            else { Console.WriteLine($"{userNum} Odd."); }
             //Output
             Console.WriteLine($"Have a good day {userName}.");
             Console.WriteLine($"(Press enter to exit)");
             Console.ReadLine();
-
         }
+
     }
 }
